735088fb26b8829ff01e652a5173c603d5047341
Lee <lee@mtftm.com>
2025-05-07 23:12:10 +0800

    fix: 修正 `package.json` 和  `config.ts`， 优化 `.gitignore` 文件，移除无用文件

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,48 @@
 // docs/.vitepress/config.ts
+import genConfig from '@project-trans/vitepress-theme-project-trans/config'
 import type { SidebarOptions } from '@project-trans/vitepress-theme-project-trans/theme'
 import type { ThemeContext } from '@project-trans/vitepress-theme-project-trans/utils'
-import genConfig from '@project-trans/vitepress-theme-project-trans/config'
 import { withThemeContext } from '@project-trans/vitepress-theme-project-trans/utils'
+import type { DefaultTheme } from 'vitepress'
 
-// 定义 nav 和 sidebarOptions
-const nav = []  // 目前定义为空数组，根据需要进行配置
-// 注意这里将 sidebarOptions 定义为一个空数组，而不是空对象
-const sidebarOptions: SidebarOptions = []
+// nav 配置
+type NavConfig = DefaultTheme.Config['nav']
+const nav: NavConfig = [
+  {
+    text: '首页',
+    link: '/',
+  },
+  // {
+  //   text: '关于',
+  //   link: '/about/',
+  // },
+]
+
+// 侧边栏配置
+const baseConfig = {
+  useTitleFromFrontmatter: true,
+  useFolderTitleFromIndexFile: true,
+  useFolderLinkFromIndexFile: true,
+  excludeFilesByFrontmatterFieldName: true,
+  collapsed: true,
+  documentRootPath: '/docs',
+} satisfies Partial<SidebarOptions>
+
+// 配置侧边栏的路径
+const sidebarOptions = [
+  // 首页
+  {
+    ...baseConfig,
+    scanStartPath: '/',
+    resolvePath: '/',
+  },
+  // 关于
+  // {
+  //   ...baseConfig,
+  //   scanStartPath: 'about',
+  //   resolvePath: '/about/',
+  // },
+]
 
 const themeConfig: ThemeContext = {
   siteTitle: 'RLE.wiki',
@@ -18,16 +53,16 @@ const themeConfig: ThemeContext = {
   nav,
   sidebarOptions,
   enableDisclaimer: true,
-  disclaimerPaths: [{
-    path: '/campus/',
-    summaryHtml: 'RLE.wiki「大学指南」中的内容，仅供参考。可能存在过时或不准确的信息，请谨慎甄别。',
-    detailHtml:
-      '<p>RLE.wiki「大学指南」中的内容，仅供参考。可能存在过时或不准确的信息，请谨慎甄别。</p>' +
-      '<p>「大学指南」板块中的内容，多数来自于读者投稿，并经编辑简单整理和形式审查后登载，主要体现其投稿者主观观点。不代表 RLE.wiki 编辑团队及我们的任何相关维护人员立场。</p>' +
-      '<p>若存在任何有误或不当内容，请联系 <a href="mailto:rlewiki@project-trans.org">rlewiki@project-trans.org</a>。</p>',
-  }],
-  disclaimerStatusKey: 'disclaimerStatus',
-  disclaimerStatusExpiration: 1000
+  // disclaimerPaths: [{
+  //   path: '/campus/',
+  //   summaryHtml: 'RLE.wiki「大学指南」中的内容，仅供参考。可能存在过时或不准确的信息，请谨慎甄别。',
+  //   detailHtml:
+  //     '<p>RLE.wiki「大学指南」中的内容，仅供参考。可能存在过时或不准确的信息，请谨慎甄别。</p>' +
+  //     '<p>「大学指南」板块中的内容，多数来自于读者投稿，并经编辑简单整理和形式审查后登载，主要体现其投稿者主观观点。不代表 RLE.wiki 编辑团队及我们的任何相关维护人员立场。</p>' +
+  //     '<p>若存在任何有误或不当内容，请联系 <a href="mailto:rlewiki@project-trans.org">rlewiki@project-trans.org</a>。</p>',
+  // }],
+  // disclaimerStatusKey: 'disclaimerStatus',
+  // disclaimerStatusExpiration: 1000
 }
 
 // https://vitepress.dev/reference/site-config
